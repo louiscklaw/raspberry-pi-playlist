17f4dde5398393333a5ead2d22e8283deb65a6d6
louiscklaw <louiscklaw@gmail.com>
2020-03-18 15:36:04 +0800

    update,

---
+++ hello_functions/hello_functions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "test_functions.c"
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	test_functions();
+	return(EXIT_SUCCESS);
+}
---
+++ hello_functions/test_functions.c
@@ -0,0 +1,5 @@
+
+int test_functions(void){
+  puts("!!!test functions!!!");
+  return 0;
+}
