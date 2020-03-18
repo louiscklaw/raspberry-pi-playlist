6c77e43a81f65a49bb7bbf886f60dc3896acb9b0
louiscklaw <louiscklaw@gmail.com>
2020-03-18 20:17:28 +0800

    update for working crc-ccitt16,

--- hello_ccitt_crc16/hello_ccitt_crc16.c
+++ hello_ccitt_crc16/hello_ccitt_crc16.c
@@ -15,7 +15,7 @@ unsigned short crc16(char *ptr, int count)
 {
    int  crc;
    char i;
-   crc = 0xffff;
+   crc = 0x0;
    while (--count >= 0)
    {
       crc = crc ^ (int) *ptr++ << 8;
@@ -35,22 +35,42 @@ unsigned short crc16(char *ptr, int count)
 
 
 int main(void) {
-	char test_input[9];
-	test_input[0]=0x31;
-	test_input[1]=0x32;
-	test_input[2]=0x33;
-	test_input[3]=0x34;
-	test_input[4]=0x35;
-	test_input[5]=0x36;
-	test_input[6]=0x37;
-	test_input[7]=0x38;
-	test_input[8]=0x39;
+	char test_input[27];
+	test_input[0] =  0x0e;
+	test_input[1] =  0x76;
+	test_input[2] =  0x6f;
+	test_input[3] =  0x6c;
+	test_input[4] =  0x74;
+	test_input[5] =  0x61;
+	test_input[6] =  0x67;
+	test_input[7] =  0x65;
+	test_input[8] =  0x0;
+	test_input[9] =  0x34;
+	test_input[10] = 0x35;
+	test_input[11] = 0x30;
+	test_input[12] = 0x30;
+	test_input[13] = 0x0;
+	test_input[14] = 0x63;
+	test_input[15] = 0x75;
+	test_input[16] = 0x72;
+	test_input[17] = 0x72;
+	test_input[18] = 0x65;
+	test_input[19] = 0x6e;
+	test_input[20] = 0x74;
+	test_input[21] = 0x0;
+	test_input[22] = 0x31;
+	test_input[23] = 0x30;
+	test_input[24] = 0x30;
+	test_input[25] = 0x30;
+	test_input[26] = 0x0;
+
+
 
 
 	printf("%c\n", test_input[1]);
 
 	puts("!!!Hello World!!!");
-	crc16(test_input, 9);
+	crc16(test_input, 27);
 
 	return(EXIT_SUCCESS);
 }
