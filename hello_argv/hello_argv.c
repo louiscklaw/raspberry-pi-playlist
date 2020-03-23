09e614d2c6cab666edeeb6ae57cd707f6cc8da69
louiscklaw <louiscklaw@gmail.com>
2020-03-23 21:59:13 +0800

    update for working changing voltage,

--- hello_argv/hello_argv.c
+++ hello_argv/hello_argv.c
@@ -41,35 +41,54 @@ unsigned short crc16(char *ptr, int count)
           crc = crc << 1;
     } while(--i);
   }
-  printf("%08x", crc);
   return (crc);
 }
 
 int change_voltage(char * value_input){
   printf("voltage to change: %s mV\n", value_input);
+  unsigned short checksum_result;
 
-	char test_input[18];
-	test_input[0] =  0x7e;
-	test_input[1] =  0x0e;
-	test_input[2] =  0x76;
-	test_input[3] =  0x6f;
-	test_input[4] =  0x6c;
-	test_input[5] =  0x74;
-	test_input[6] =  0x61;
-	test_input[7] =  0x67;
-	test_input[8] =  0x65;
-	test_input[9] =  0x00;
-	test_input[10] = value_input[0];
-	test_input[11] = value_input[1];
-	test_input[12] = value_input[2];
-	test_input[13] = value_input[3];
-	test_input[14] = 0x00;
+  char command_start[1];
+  command_start[0] = 0x7e;
 
-  crc16(test_input, 15);
+  char command_end[1];
+  command_end[0]=0x7f;
 
-	test_input[15] = 0x87;
-	test_input[16] = 0xd5;
-	test_input[17] = 0x7f;
+	char test_input[16];
+	test_input[0 ] =  0x0e;
+	test_input[1 ] =  0x76;
+	test_input[2 ] =  0x6f;
+	test_input[3 ] =  0x6c;
+	test_input[4 ] =  0x74;
+	test_input[5 ] =  0x61;
+	test_input[6 ] =  0x67;
+	test_input[7 ] =  0x65;
+	test_input[8 ] =  0x00;
+	test_input[9 ] = value_input[0];
+	test_input[10] = value_input[1];
+	test_input[11] = value_input[2];
+	test_input[12] = value_input[3];
+	test_input[13] = 0x00;
+
+  checksum_result = crc16(test_input, 14);
+  // unsigned short test1;
+  // char test_checksum[4];
+  // test1=0x1234;
+
+  // printf("%04x", test1);
+  // sprintf(test_checksum,"%x", );
+  // test_checksum[0] = test1;
+  // test_checksum[1] = test1 ;
+  printf("%02x\n", checksum_result & 0xff00 );
+  printf("%02x\n", checksum_result & 0x00ff);
+
+	// test_input[14] = 0x87;
+	// test_input[15] = 0xd5;
+
+	test_input[14] = checksum_result >> 8;
+	test_input[15] = checksum_result & 0x00ff;
+
+	// test_input[17] = 0x7f;
 
 	// crc16(test_input, 18);
   int fd;
@@ -81,11 +100,18 @@ int change_voltage(char * value_input){
 
   fcntl(fd, F_SETFL, 0);
 
+  write_to_the_port(fd, command_start[0]);
+  printf("%02x", command_start[0]);
+
   int i = 0;
-  for(i=0;i<18;i++)
+  for(i=0;i<16;i++)
   {
+    printf("%02x", test_input[i]);
     write_to_the_port(fd, test_input[i]);
   }
+
+  printf("%02x", command_end[0]);
+  write_to_the_port(fd, command_end[0]);
 
   close(fd);
 
