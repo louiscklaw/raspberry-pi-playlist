f0d3e02551178b0ab633eb8e3a5dc829af1bae0c
louiscklaw <louiscklaw@gmail.com>
2022-01-21 16:53:52 +0800

    update,

--- hosts/RPIServer1/overlay/etc/nginx/sites-enabled/install_rpi_monitor.sh
+++ hosts/RPIServer1/overlay/etc/nginx/sites-enabled/install_rpi_monitor.sh
@@ -12,3 +12,14 @@ sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
 # To install RPi-Monitor, execute the following command:
 sudo apt-get update
 sudo apt-get install rpimonitor
+
+# sudo apt-get install nginx
+sudo apt-get install nginx
+
+sudo mkdir -p /etc/ssl/localcerts
+openssl req -new -x509 -days 3650 -nodes -out \
+/etc/ssl/localcerts/RPi-Experiences-cert.pem -keyout \
+/etc/ssl/localcerts/RPi-Experiences-key.pem
+chmod 600 /etc/ssl/localcerts/*
+
+sudo rm /etc/nginx/sites-enable/default
